0cee47ec452a1c2fc57b0c2f2e6ff0c802808ba0
André König <andre.koenig@gmail.com>
2014-03-07 22:10:46 +0100

    Initial commit. Created directory structure.

---
+++ README.md
@@ -0,0 +1,7 @@
+# nmg - node module generator
+
+A community-driven node module generator.
+
+# Author
+
+2014, André König (andre.koenig@posteo.de)
---
+++ index.js
@@ -0,0 +1,29 @@
+/*
+ * nmg - node module generator
+ *
+ * Copyright(c) 2014 André König <andre.koenig@posteo.de>
+ * MIT Licensed
+ *
+ */
+
+/**
+ * @author André König <andre.koenig@posteo.de>
+ *
+ */
+
+'use strict';
+
+var pkg       = require('./package.json'),
+    Generator = require('./lib');
+
+/**
+ * DOCME
+ *
+ * @param  {[type]} templateName [description]
+ * @param  {[type]} options      [description]
+ * @return {[type]}              [description]
+ *
+ */
+exports.generate = function generate (templateName, options) {
+    
+};
