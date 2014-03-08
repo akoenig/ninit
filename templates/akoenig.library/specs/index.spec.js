19f9d1c63c7a716c2a3474ecf6d56df4a1a610f0
André König <andre.koenig@gmail.com>
2014-03-08 01:59:54 +0100

    Integrated spec.

---
+++ templates/akoenig.library/specs/index.spec.js
@@ -0,0 +1,21 @@
+/*
+ * @name@
+ *
+ * Copyright(c) 2014 @your name@ <@your email address@>
+ * MIT Licensed
+ *
+ */
+
+/**
+ * @author @your name@ <@your email address@>
+ *
+ */
+
+'use strict';
+
+describe('The "@name@"', function () {
+
+    it('should ...', function (done) {
+
+    });
+});
